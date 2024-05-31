700fabe292a925dbd673733b81191efc775ae4a7
Jessica Espejo <jenespejoto@unal.edu.co>
2024-05-30 23:07:07 -0500

    Configuracion front de mostrar opciones de suscripcion y listar suscripcion

--- src/routes/subscriptions.routes.js
+++ src/routes/subscriptions.routes.js
@@ -1,16 +1,20 @@
-import {Router} from 'express';
+import { Router } from 'express';
 import pool from '../database.js';
 
 const subscRouter = Router();
 
-subscRouter.get('/list-subsc', async(req,res) => {
-    try{
+subscRouter.get('/list-subsc', async (req, res) => {
+    try {
         const [result] = await pool.query('SELECT * FROM suscribers');
-        res.render('subscriptions/list-subsc', {suscribers:result})
+        res.render('subscriptions/list-subsc', { suscribers: result })
 
-    }catch(error){
-        res.status(500).json({message: error.message});
+    } catch (error) {
+        res.status(500).json({ message: error.message });
     }
+});
+
+subscRouter.get('/show-subsc', (req, res) => {
+    res.render('subscriptions/show-subsc');
 });
 
 export default subscRouter;
