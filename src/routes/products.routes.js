8ae8a770cb19877197912306b50ef493aafe9e51
Jessica Espejo <jenespejoto@unal.edu.co>
2024-06-01 02:12:30 -0500

    configuraciion add-prod

--- src/routes/products.routes.js
+++ src/routes/products.routes.js
@@ -13,7 +13,26 @@ prodRouter.get('/list-prod',async(req,res) => {
     }
 });
 
+prodRouter.get('/add-prod', (req,res) => {
+    res.render('products/add-prod');
+});
 
+
+prodRouter.post('/add-prod',  async(req,res) => {
+    try{
+        const {prodname,size,color,brand,description,price} = req.body;
+        let newProduct= {
+                prodname,size,color,brand,description,price
+            }
+        
+        await pool.query('INSERT INTO products SET ?', [newProduct]);
+        
+        res.redirect('/list-prod');
+
+    }catch(error){
+        res.status(500).json({message: error.message});
+    }
+});
 
 
 export default prodRouter;
