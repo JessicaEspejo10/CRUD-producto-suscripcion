85d560e36470c33aae1aec9bb0ab80fbcf3c20c2
Jessica Espejo <jenespejoto@unal.edu.co>
2024-05-31 22:46:57 -0500

    configuracion de add-subsc, ajustes de list-add y show-add

--- src/database/tables.sql
+++ src/database/tables.sql
@@ -4,20 +4,20 @@ USE Adventure;
 
 CREATE TABLE products(
     id INT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(50) NOT NULL,
+    prodname VARCHAR(50) NOT NULL,
     location VARCHAR(50) NOT NULL,
     observation VARCHAR(200) NOT NULL,
     travel_date DATE NOT NULL,
-    duration INT NOT NULL,
+    duration INT NOT NULL
 );
 
 CREATE TABLE suscribers(
     id INT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(50) NOT NULL,
+    subscname VARCHAR(50),
     lastname VARCHAR(50) NOT NULL,
+    email VARCHAR(50) NOT NULL,
     suscription_type VARCHAR(50) NOT NULL,
-    billing_date VARCHAR(50) NOT NULL,
-    expiration_date VARCHAR(50) NOT NULL,
+    value INT,
+    billing_date VARCHAR(50) NOT NULL
     );
 
-SELECT * FROM products;
--- src/routes/subscriptions.routes.js
+++ src/routes/subscriptions.routes.js
@@ -17,4 +17,34 @@ subscRouter.get('/show-subsc', (req, res) => {
     res.render('subscriptions/show-subsc');
 });
 
+
+subscRouter.get('/add-subsc', (req, res) => {
+    res.render('subscriptions/add-subsc', { value: req.query.value, suscription_type: req.query.suscription_type });
+    console.log(req.body)
+});
+
+subscRouter.post('/add-subsc', async (req, res) => {
+    
+    //const { value, type } = req.body;
+    //res.send(`Valores recibidos: ${value}, ${type}`);
+
+    try {
+
+        const { subscname, lastname, email, suscription_type, value, billing_date } = req.body;
+        
+        console.log(req.body);
+        let newSuscriber = {
+            
+            subscname, lastname, email, suscription_type, value, billing_date
+        }
+        
+        await pool.query('INSERT INTO suscribers SET ?', [newSuscriber]);
+        res.json({ redirectUrl: '/list-subsc' });
+    }
+    catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+});
+
+
 export default subscRouter;
